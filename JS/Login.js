0a5aeb655bdd5e9beddb3bb238cb5f048b07e41a
unknown <janneth_vc@hotmail.com>
2024-07-16 23:21:29 -0500

    Version_1

---
+++ JS/Login.js
@@ -0,0 +1,17 @@
+const usuario=document.getElementById('usuario')
+const password=document.getElementById('password')
+const button=document.getElementById('button')
+
+function loguear(){
+    let usuario=document.getElementById("usuario").value;
+    let password=document.getElementById("password").value;
+    if (usuario==="Clean" && password==="123456")
+        {
+            window.location.href="Servicios.html"
+        }
+        else
+        {alert("Ha ingreso los usuario/clave incorrectos");
+        }
+}
+
+loguear();
